a7cf6a44ccf02248a3c55feb77cd44db4637e99e
Michael Salerno <msalerno1993@gmail.com>
2024-05-12 14:33:06 -0400

    connected to mongo db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
+// const Contact = require("./Models/contactModel");
+const cors = require("cors")
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const startServer = async () => {
+    try {
+      await mongoose.connect(process.env.CONNECTIONSTRING);
+      console.log("Connected to MongoDB");
+  
+      app.listen(process.env.PORT, () =>
+        console.log(`Example app listening on port ${process.env.PORT}!`)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+  startServer();
